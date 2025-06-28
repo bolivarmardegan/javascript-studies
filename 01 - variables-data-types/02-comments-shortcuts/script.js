4d65139cf5ddf1b053d8f93f24fd1ab87cd63952
bolivarmardegan <bolivar.android@gmail.com>
2025-06-28 15:08:01 -0300

    feature: commnets and shortcuts

---
+++ 01 - variables-data-types/02-comments-shortcuts/script.js
@@ -0,0 +1,20 @@
+console.log(100)
+
+saudacao = "Hello"
+
+palemiras_nao_tem_mundial = true
+
+x = (100 - 90)
+
+// console.group('simple')
+// console.log(x, "palmeiras não tem mundia: " + palemiras_nao_tem_mundial, saudacao)
+// console.error('ALERT')
+// console.warn('warn')
+// console.table({name : 'Bolivar', email:'bolivar@gmail.com'})
+// console.groupEnd()
+
+/*
+const styles = 'padding: 10px; background-color: white; color: green;'
+
+console.log('%cEstilos:', styles)
+*/
