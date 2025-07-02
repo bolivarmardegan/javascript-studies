3d779e6576aa4a7243ace46386ed5ea57278e5b6
bolivarmardegan <bolivar.android@gmail.com>
2025-07-01 22:16:57 -0300

    feature: variables

---
+++ 01 - variables-data-types/04-data-types/script.js
@@ -0,0 +1,37 @@
+//String
+const firstName = 'Sarah'
+
+//Number
+const age = 43;
+const temp = 99.0
+
+//Boolean
+const isTrue = true
+
+//Null
+const house = null
+
+//Undefined
+// let tu;
+const term = undefined
+
+//Symbol
+const id = Symbol('uuid')
+
+//BigINt
+const nu = 9999999999999n
+
+//Reference types
+const numbers = [1,2,3,4,5]
+
+const cars = {
+  'model':'Ferrari',
+  'year':1980
+}
+
+function sayHello(){
+  console.log('Hello')
+}
+
+const output = sayHello
+console.log(output, typeof output )
