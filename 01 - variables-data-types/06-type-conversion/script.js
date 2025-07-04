ab77ad28f58988079b67d054775eca38bf4fc5ea
bolivarmardegan <bolivar.android@gmail.com>
2025-07-03 22:31:16 -0300

    convertions

---
+++ 01 - variables-data-types/06-type-conversion/script.js
@@ -0,0 +1,20 @@
+let amount = 1
+
+// Convert string to Number
+// amount = parseInt(amount)
+// amount = +amount
+// amount = Number(amount)
+
+// Convert number to string
+// amount = amount.toString()
+// amount = String(amount)
+
+
+//Convert string to decimal
+// amount = parseFloat(amount)
+
+//Convert number to bolean
+amount = Boolean(amount)
+
+
+console.log(amount, typeof amount)
