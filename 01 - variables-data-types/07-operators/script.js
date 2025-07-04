7ac34d23f87440737147d5ad7bb60c0331e134dc
bolivarmardegan <bolivar.android@gmail.com>
2025-07-03 22:58:05 -0300

    operators

---
+++ 01 - variables-data-types/07-operators/script.js
@@ -0,0 +1,34 @@
+let x;
+
+x = 5 + 5
+x = 5 - 5
+x = 5 * 5
+x = 5 / 5
+
+//Expoent
+// x = 2 ** 3
+
+
+//Incremment
+x++
+
+//Decremment
+x--
+
+x += 10
+x -= 1
+// x %= 3
+
+ x **= 3
+
+ //Comparsion Operators
+
+ y = 2 == '2'// return true, because == doesn't compare the type of the variable
+ y = 2 === '2' // The result is false, because === compare type of the variable.
+
+
+ y = 2 != '2'
+//  y = 2 !== '2'
+
+console.log(x)
+console.log(y)
